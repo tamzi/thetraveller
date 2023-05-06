7efa821f9d715d3a362a84d750e6507c8b154e77
tamzi <tamrefrank@gmail.com>
2023-05-06 19:41:22 +0200

    upgrades

--- app/src/main/java/com/tamzi/thetraveller/MainActivity.kt
+++ app/src/main/java/com/tamzi/thetraveller/MainActivity.kt
@@ -3,10 +3,12 @@ package com.tamzi.thetraveller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tamzi.thetraveller.ui.theme.ThetravellerTheme
 
@@ -16,22 +18,29 @@ class MainActivity : ComponentActivity() {
         setContent {
             ThetravellerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("The traveller")
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun GreetingPreview() {
     ThetravellerTheme {
         Greeting("Android")
     }
